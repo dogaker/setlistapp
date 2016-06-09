585b5266f7c2902e8ae869d00d412ce8719515d5
dogaker <doga@sas.upenn.edu>
2016-06-09 18:11:15 -0400

    First Commit

--- setlistdata.py
+++ setlistdata.py
@@ -1,24 +1,32 @@
-def setlistdata(mbid):
-    # Working code for downloading all of the data of setlists from setlist.fm for an artist
+# For getting the json dump for all the setlists of a band
 
-    setlist = urllib2.urlopen('http://api.setlist.fm/rest/0.1/artist/' + mbid + '/setlists.json?p=1')
+
+import urllib2
+import json
+import time
+
+
+def setlistdata(mbid):
+    """Connects to the api and downloads all the setlists data for a band
+    using the the mbid"""
+
+    setlist = urllib2.urlopen('http://api.setlist.fm/rest/0.1/artist/' +
+                              mbid + '/setlists.json?p=1')
     data = json.load(setlist)
 
     totalshows = int(data['setlists']['@total'])
-    setlistsongsdetail = []
-    setname = []
     print totalshows
-
 
     pages = int(totalshows/20)
     print pages
+
     data = []
     time.sleep(0.5)
-    for page in range(1,pages):
+    for page in range(1, pages):
         time.sleep(0.5)
-        setlist = urllib2.urlopen('http://api.setlist.fm/rest/0.1/artist/' + mbid + '/setlists.json?p=' + str(page))
+        setlist = urllib2.urlopen('http://api.setlist.fm/rest/0.1/artist/' +
+                                  mbid + '/setlists.json?p=' + str(page))
         data.append(json.load(setlist))
-
 
     print "done"
     return data
