a76f0814a951e440ab698fba8128edd96392d1bc
dogaker <doga@sas.upenn.edu>
2016-06-10 01:34:24 -0400

    added comments for future work

--- lastfmuserhistory.py
+++ lastfmuserhistory.py
@@ -1,3 +1,8 @@
+# I think we can seriously improve the speed here if I download the entire
+# data first and then run through it. the delay is slowing things down but
+# still I think there is a bottleneck with actually running the calculations
+# as well
+
 import json
 import urllib2
 import time
@@ -24,41 +29,24 @@ def lastfmuserhistory(username, artistname, apikey):
 
         # this if clause is what breaks the data when the new page that is
         # loaded in the api has no more new information to report
-
         if data['artisttracks']['track'] == []:
             # print "success!!!"
             break
 
-
-    # debugging code for test cases where I knew how many pages were supposed to load in total
-    #     if page >= 16:
-    #         print "page = 20. we failed!"
-    #         break
-
-
     # this is where the code that actually scrapes the data begins
-    # what this function does is given the artist name it gets the most listened tracks by the user.
-    # it also gets the mbid code which is the standard code reported in all the API I'm using
-
+    # what this function does is given the artist name it gets the most
+    # listened tracks by the user. also gets mbid code
         else:
-
             n = 0
             page += 1
             for inf in data['artisttracks']['track']:
                 time.sleep(0.5)
                 if mbid == []:
                     mbid = data['artisttracks']['track'][n]['artist']['mbid']
+
     # the mbidold here is mainly for debugging purposes in case there is multiple mbid's coded for one artist in last.fm
                     mbidold = mbid
-    #                 trackname = {'trackname': data['artisttracks']['track'][n].get('name')}
-    #                 albumname = {'albumname': data['artisttracks']['track'][n].get('name')}
-
-#                     songslistened.append([{'trackname': data['artisttracks']['track'][n].get('name')}, {'albumname': data['artisttracks']['track'][n].get('name')}, {'time': data['artisttracks']['track'][n]['date'].get('#text')}, {'mbid': data['artisttracks']['track'][n]['artist'].get('mbid')}])
-                    songslistened.update({"track"+str(track):{'trackname': data['artisttracks']['track'][n].get('name'),
-                                         'albumname': data['artisttracks']['track'][n].get('name'),
-                                         'time': data['artisttracks']['track'][n]['date'].get('#text'),
-                                         'mbid': data['artisttracks']['track'][n]['artist'].get('mbid')}})
-                    # songslistened.update(songs)
+                    songslistened.update({"track"+str(track): {'trackname': data['artisttracks']['track'][n].get('name'), 'albumname': data['artisttracks']['track'][n].get('name'), 'time': data['artisttracks']['track'][n]['date'].get('#text'), 'mbid': data['artisttracks']['track'][n]['artist'].get('mbid')}})
                     print data['artisttracks']['track'][n]['artist']['mbid']
                     print data['artisttracks']['track'][n]['name'], data['artisttracks']['track'][n]['album']['#text'], data['artisttracks']['track'][n]['date']['#text']
                     n += 1
@@ -66,26 +54,17 @@ def lastfmuserhistory(username, artistname, apikey):
 
                 else:
                     if data['artisttracks']['track'][n]['artist']['mbid'] == mbidold:
-#                         songslistened.append([{'trackname': data['artisttracks']['track'][n].get('name')}, {'albumname': data['artisttracks']['track'][n].get('name')}, {'time': data['artisttracks']['track'][n]['date'].get('#text')}, {'mbid': data['artisttracks']['track'][n]['artist'].get('mbid')}])
-#                         songs = {"track"+str(track):{'trackname': data['artisttracks']['track'][n].get('name'), 'albumname': data['artisttracks']['track'][n].get('name'), 'time': data['artisttracks']['track'][n]['date'].get('#text'), 'mbid': data['artisttracks']['track'][n]['artist'].get('mbid')}}
-#                         songslistened.update(songs)
                         songslistened.update({"track"+str(track):{'trackname': data['artisttracks']['track'][n].get('name'), 'albumname': data['artisttracks']['track'][n].get('name'), 'time': data['artisttracks']['track'][n]['date'].get('#text'), 'mbid': data['artisttracks']['track'][n]['artist'].get('mbid')}})
                         print data['artisttracks']['track'][n]['name'], data['artisttracks']['track'][n]['album']['#text'], data['artisttracks']['track'][n]['date']['#text']
                         n += 1
                         track += 1
 
-
-
-
                     else:
                         mbid = data['artisttracks']['track'][n]['artist']['mbid']
-#                         songslistened.append([{'trackname': data['artisttracks']['track'][n].get('name')}, {'albumname': data['artisttracks']['track'][n].get('name')}, {'time': data['artisttracks']['track'][n]['date'].get('#text')}, {'mbid': data['artisttracks']['track'][n]['artist'].get('mbid')}])
-#                         songs = {"track"+str(track):{'trackname': data['artisttracks']['track'][n].get('name'), 'albumname': data['artisttracks']['track'][n].get('name'), 'time': data['artisttracks']['track'][n]['date'].get('#text'), 'mbid': data['artisttracks']['track'][n]['artist'].get('mbid')}}
-#                         songslistened.update(songs)
                         songslistened.update({"track"+str(track):{'trackname': data['artisttracks']['track'][n].get('name'), 'albumname': data['artisttracks']['track'][n].get('name'), 'time': data['artisttracks']['track'][n]['date'].get('#text'), 'mbid': data['artisttracks']['track'][n]['artist'].get('mbid')}})
                         print "Error! mbid conflict! old mbid: " + str(mbidold), "new mbid: " + str(mbid)
                         print data['artisttracks']['track'][n]['name'], data['artisttracks']['track'][n]['album']['#text'], data['artisttracks']['track'][n]['date']['#text']
-                        n+=1
-                        track +=1
+                        n += 1
+                        track += 1
     print "Success!"
     return mbid, songslistened
