72a1dea0518fa6a290f739b06da38f7ace48e6dd
dogaker <doga@sas.upenn.edu>
2016-06-24 16:03:40 -0400

    clean up and returns df instead of dict

--- algorithm.py
+++ algorithm.py
@@ -4,68 +4,47 @@ from sklearn.cross_validation import train_test_split
 from sklearn.metrics import roc_auc_score, roc_curve, auc
 import pandas as pd
 
-def predictsetlist(data):
 
-    df = data
-    df = df.sort_values(by = 'eventDate')
+def predictsetlist(df):
+
+    df = df.sort_values(by='eventDate')
     df = df.reset_index(drop=True)
     df['played'] = 1
     grouped = df.groupby('eventID')
 
-    df['new_song']= 0
-    df['old_song']= 0
+    df['new_song'] = 0
+    df['old_song'] = 0
     songs = []
     newsongscount = 0
-    for index, row in df.iterrows():
+    df.sort_values(by='eventDate')
+    df['new_song'] = (df.groupby('shorttrackname').cumcount() == 0).astype(int)
+    df['old_song'] = (df.groupby('shorttrackname').cumcount() != 0).astype(int)
+    df = df[df.shorttrackname != '']
+    hashnames = df.set_index('shorttrackname')['songname'].to_dict()
+    livesonglist = list(hashnames.keys())
+    hashedevents = df.set_index('eventID')['venueID'].to_dict()
+    eventidlist = list(hashedevents.keys())
 
-        if row['shorttrackname'] not in songs:
-            df.loc[index,'new_song'] = 1
-            songs.append(row['shorttrackname'])
-            newsongscount+=1
-            row['new_song']
-#            print songs
-#            print newsongscount
-        else:
-            df.loc[index,'old_song'] = 1
-
-    livesonglist = []
-    for index, row in df.iterrows():
-        if row['shorttrackname'] not in livesonglist:
-            livesonglist.append(row['shorttrackname'])
-    if "" in livesonglist:
-        livesonglist.remove('')
-#    len(livesonglist)
-
-
-    eventidlist = []
-    for index, row in df.iterrows():
-        if row['eventID'] not in eventidlist:
-            eventidlist.append(row['eventID'])
-    #eventidlist
-    #len(eventidlist)
-
-    live_song_list_full = len(eventidlist)*livesonglist
-    #len(live_song_list_full)
+    live_song_list_full = len(eventidlist) * livesonglist
+    # len(live_song_list_full)
     live_song_full_df = pd.DataFrame({'shorttrackname': live_song_list_full})
     live_song_full_df = live_song_full_df.reset_index()
-    event_id_list_full = len(livesonglist)*eventidlist
+    event_id_list_full = len(livesonglist) * eventidlist
     #len(live_song_list_full), len(event_id_list_full)
-
 
     event_id_list_full_df = pd.DataFrame({'eventID': event_id_list_full})
 
-    event_id_list_full_df = event_id_list_full_df.sort_values(by='eventID').reset_index()
+    event_id_list_full_df = event_id_list_full_df.sort_values(
+        by='eventID').reset_index()
     event_id_list_full_df = event_id_list_full_df.drop('index', axis=1)
     event_id_list_full_df = event_id_list_full_df.reset_index()
 
+    mergeleft = event_id_list_full_df.merge(live_song_full_df, on='index')
+    mergeleft['eventtrack'] = mergeleft[
+        'eventID'] + mergeleft['shorttrackname']
 
-
-    mergeleft = event_id_list_full_df.merge(live_song_full_df, on = 'index')
-    mergeleft['eventtrack'] = mergeleft['eventID']+mergeleft['shorttrackname']
-
-    df['eventtrack']=df['eventID']+df['shorttrackname']
+    df['eventtrack'] = df['eventID'] + df['shorttrackname']
 #    df = df.drop_duplicates(subset='eventtrack', keep='first')
-
 
     df_full = pd.merge(mergeleft, df, on='eventtrack', how='left')
     df_full = df_full[df_full.eventID_x != 0]
@@ -82,33 +61,47 @@ def predictsetlist(data):
     grouped_df_full = df_full.groupby('eventID_x')
 #    print len(grouped_df_full)
 
-    df_full['eventDate'] = grouped_df_full['eventDate'].transform(lambda s: s.loc[s.first_valid_index()])
-    df_full['artistName'] = grouped_df_full['artistName'].transform(lambda s: s.loc[s.first_valid_index()])
-    df_full['tour'] = grouped_df_full['tour'].transform(lambda s: s.loc[s.first_valid_index()])
-    df_full['venueName'] = grouped_df_full['venueName'].transform(lambda s: s.loc[s.first_valid_index()])
-    df_full['city'] = grouped_df_full['city'].transform(lambda s: s.loc[s.first_valid_index()])
-    df_full['country'] = grouped_df_full['country'].transform(lambda s: s.loc[s.first_valid_index()])
-    df_full['countryCode'] = grouped_df_full['countryCode'].transform(lambda s: s.loc[s.first_valid_index()])
-    df_full['eventDate'] = grouped_df_full['eventDate'].transform(lambda s: s.loc[s.first_valid_index()])
-    df_full['mbID'] = grouped_df_full['mbID'].transform(lambda s: s.loc[s.first_valid_index()])
-    df_full['stateCode'] = grouped_df_full['stateCode'].transform(lambda s: s.loc[s.first_valid_index()])
+    df_full['eventDate'] = grouped_df_full['eventDate'].transform(
+        lambda s: s.loc[s.first_valid_index()])
+    df_full['artistName'] = grouped_df_full['artistName'].transform(
+        lambda s: s.loc[s.first_valid_index()])
+    df_full['tour'] = grouped_df_full['tour'].transform(
+        lambda s: s.loc[s.first_valid_index()])
+    df_full['venueName'] = grouped_df_full['venueName'].transform(
+        lambda s: s.loc[s.first_valid_index()])
+    df_full['city'] = grouped_df_full['city'].transform(
+        lambda s: s.loc[s.first_valid_index()])
+    df_full['country'] = grouped_df_full['country'].transform(
+        lambda s: s.loc[s.first_valid_index()])
+    df_full['countryCode'] = grouped_df_full['countryCode'].transform(
+        lambda s: s.loc[s.first_valid_index()])
+    df_full['eventDate'] = grouped_df_full['eventDate'].transform(
+        lambda s: s.loc[s.first_valid_index()])
+    df_full['mbID'] = grouped_df_full['mbID'].transform(
+        lambda s: s.loc[s.first_valid_index()])
+    df_full['stateCode'] = grouped_df_full['stateCode'].transform(
+        lambda s: s.loc[s.first_valid_index()])
     df_full['played'] = grouped_df_full['played'].fillna(0)
-    df_full['new_song'] = grouped_df_full['new_song'].fillna(0) # I need to change this so that it becomes left censored
+    # I need to change this so that it becomes left censored
+    df_full['new_song'] = grouped_df_full['new_song'].fillna(0)
 
-    df_full = df_full.sort_values(by = ['eventDate', 'eventID_x', 'order']).reset_index()
+    df_full = df_full.sort_values(
+        by=['eventDate', 'eventID_x', 'order']).reset_index()
 
-    shorttrackname_x_dum = pd.get_dummies(df_full['shorttrackname_x'], prefix='song')
-    tour_dum = pd.get_dummies(df_full['tour'], prefix = 'tour')
-    eventID_x_dum = pd.get_dummies(df_full['eventID_x'], prefix = 'id')
-    city_dum = pd.get_dummies(df_full['city'], prefix = 'city')
-    country_dum = pd.get_dummies(df_full['countryCode'], prefix = 'country')
+    shorttrackname_x_dum = pd.get_dummies(
+        df_full['shorttrackname_x'], prefix='song')
+    tour_dum = pd.get_dummies(df_full['tour'], prefix='tour')
+    eventID_x_dum = pd.get_dummies(df_full['eventID_x'], prefix='id')
+    city_dum = pd.get_dummies(df_full['city'], prefix='city')
+    country_dum = pd.get_dummies(df_full['countryCode'], prefix='country')
 
-    result = pd.concat([df_full, eventID_x_dum, tour_dum, city_dum, country_dum, shorttrackname_x_dum], axis=1)
+    result = pd.concat([df_full, eventID_x_dum, tour_dum,
+                        city_dum, country_dum, shorttrackname_x_dum], axis=1)
 
     cols = [[col for col in list(result) if col.startswith('tour_')],
-              [col for col in list(result) if col.startswith('id_')],
-              [col for col in list(result) if col.startswith('country_')],
-              [col for col in list(result) if col.startswith('song_')]]
+            [col for col in list(result) if col.startswith('id_')],
+            [col for col in list(result) if col.startswith('country_')],
+            [col for col in list(result) if col.startswith('song_')]]
 
     flattened_cols = [val for sublist in cols for val in sublist]
     modeldata = result[flattened_cols]
@@ -125,24 +118,45 @@ def predictsetlist(data):
     rf.fit(train1x, train1y.ravel())
 
     results = rf.predict(testx)
+    probabilities = rf.predict_proba(testx)
+    probabilities = pd.DataFrame(probabilities)
+
+    probabilities['results'] = results
+    test2 = test1.reset_index()
+    probabilities = test2.join(probabilities)
+    probabilities = probabilities.rename(columns={1: 'probability'})
+    probabilities = probabilities.reset_index()
+    df_full = df_full.rename(columns={"index": "oldindex"})
+    df_full = df_full.reset_index()
+
+    temp = df_full.merge(probabilities, on='index')
+    temp
+    predictions = temp[['shorttrackname_x', 'probability', 'results']]
+    predictions['songname'] = predictions['shorttrackname_x']
+    predictions = predictions.replace({'songname': hashnames})
+    predictions['probabilitypercentage'] = pd.Series(["{0:.2f}%".format(
+        val * 100) for val in predictions['probability']], index=predictions.index)
+
     test1['predictions'] = results
     test1.head()
 
     y_true = test1['played']
     y_scores = test1['predictions']
     roc_score = roc_auc_score(y_true, y_scores)
-    songspredicted = test1[test1['predictions']>0]
+    songspredicted = test1[test1['predictions'] > 0]
 
     songspredicted = songspredicted.loc[:, (songspredicted != 0).any(axis=0)]
-    songspredicted = [col for col in list(songspredicted) if col.startswith('song_')]
+    songspredicted = [col for col in list(
+        songspredicted) if col.startswith('song_')]
     print songspredicted
     if songspredicted:
 
         songspredicted = {'predictedsongs': songspredicted}
         songspredicted = pd.DataFrame(songspredicted)
-        songspredicted['predictedsongs'] = songspredicted['predictedsongs'].str.replace('song_', '')
-        songspredicted['predictedsongs'] = songspredicted['predictedsongs'].str.replace('_', ' ').str.title()
-        return songspredicted, roc_score
+        songspredicted['predictedsongs'] = songspredicted[
+            'predictedsongs'].str.replace('song_', '')
+#        songspredicted['predictedsongs'] = songspredicted['predictedsongs'].str.replace('_', ' ').str.title()
+        return songspredicted, roc_score, hashnames, predictions
     else:
         songspredicted = "I can't predict"
-        return songspredicted, roc_score
+        return songspredicted, roc_score, hashnames, predictions
